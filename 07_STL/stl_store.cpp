2acb4b6f6d8d6f1039d980f87d48579b3c880671
VIN1902 <vindora1904@gmail.com>
2025-09-30 13:23:34 +0530

    add stl

--- 07_STL/stl_store.cpp
+++ 07_STL/stl_store.cpp
@@ -1,3 +1,5 @@
+// overlook for containers aspect of STL
+
 #include <iostream>
 #include <vector>
 #include <deque>
@@ -18,7 +20,7 @@ class - private members by default and used when dealing with complex entities, 
 */
 
 //in project/dev we usually do something like this where we define the structure/schema/model of 'thing' first.
-struct Products{
+struct Product{
     int productID;
     string name;
     string category;
@@ -31,3 +33,53 @@ struct Order{
     string customerID;
     time_t orderDate;
 };
+
+// vector (sequence container || continous memory storage) truly shines when using customized-datastructure (like Struct) and not just simple int or string sequences.
+int main(){
+
+    vector<Product> products = {
+        {101,"Laptop","Electronics"},
+        {102,"Mouse","Electronics"},
+        {103,"Mobile","Electronics"},
+        {104,"Plate","Utensils"},
+        {105,"Cups","Utensils"},
+        {106,"Pen","Stationary"},
+        {107,"Notepad","Stationary"},
+    };
+
+    deque<string> recentCustomers = {"C001","C002","C003","C004","C005"};
+    recentCustomers.push_back("C006");
+
+    list<Order> orderHistory;
+    orderHistory.push_back({1,101,1,"C001",time(0)});
+    orderHistory.push_back({2,102,2,"C002",time(0)});
+    orderHistory.push_back({3,103,1,"C003",time(0)});
+
+    // set has only unique values
+    set<string> categories;
+    for (const auto &product : products){
+        categories.insert(product.category);
+    }
+
+    // map has key value pair, good for keeping stock track.
+    map<int, int> productStock = {
+        {101,10},{102,20},{103,15},{104,5},{105,25},{106,16},{107,12}
+    };
+
+    multimap<string, Order> customerOrders;
+    for (const auto &order : orderHistory){
+        customerOrders.insert({order.customerID, order});
+    }
+
+    //normal map is sorted in memory space but unordered_map is very fast look-up and efficient.
+    unordered_map<string, string> customerData = {
+        {"C001", "Alice"},{"C002", "Max"},{"C003", "Bob"},{"C004", "Sakiv"},{"C005", "Harsh"},{"C006", "Harry"}
+    };
+
+    unordered_set<int> uniqueProductIDs;
+    for (const auto &product : products){
+        uniqueProductIDs.insert(product.productID);
+    }
+
+    return 0;
+}
